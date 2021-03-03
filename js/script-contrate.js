5ef35a9df8bbc2b6ee9395a5e1b19ffeead10bd3
Edie Johnny <edie.eu@gmail.com>
2021-03-03 14:45:33 -0300

    Checagem de status

--- js/script-contrate.js
+++ js/script-contrate.js
@@ -3,12 +3,14 @@
 // O CLIENTE VAI PODER ENXERGAR TODOS OS PASSOS
 //$(document).ready(function(){
 
+var cpf;
+
 $(".passo-dois").hide();
 $(".passo-tres").hide();
 $(".passo-quatro").hide();
 
 $( "#passo-um" ).click(function() {
-  var cpf = busca = $("#cpf").val().toUpperCase().replace(/[^a-zA-Z0-9 çÇáÁéÉíÍóÓúÚãÃõÕ]/g, "");
+  cpf = busca = $("#cpf").val().toUpperCase().replace(/[^a-zA-Z0-9 çÇáÁéÉíÍóÓúÚãÃõÕ]/g, "");
   var data = { busca:cpf, tipo:"todos" }
   $.ajax({
     method: "POST",
@@ -123,6 +125,31 @@ $( "#passo-tres" ).click(function() {
     $( ".passo-dois").hide();
     $( ".barra-conversao").hide();
 });
+
+function setCookie(name,value,days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days*24*60*60*1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+function eraseCookie(name) {   
+  document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
 
 
 //});
