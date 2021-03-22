ea551729b5d6ae10f9ec642183e610a985dad07f
gabrielymendes19 <gabrielymendesmontes@gmail.com>
2021-03-22 16:47:33 -0300

    gitignore

--- contrate-avulso/verificaCPF.php
+++ contrate-avulso/verificaCPF.php
@@ -51,7 +51,7 @@
       </div>
       <div class="container text-center mt-4">
         <div class=" col-md-3  mx-auto text-center">
-          <input type="text" class="form-control form-control-contate text-center" id="cpf" val="" placeholder="000.000.000-00" aria-label="Insira seu CPF" maxlength="11" aria-describedby="submit-button" required>
+          <input type="text" class="form-control form-control-contate text-center" id="cpf" val="" placeholder="000.000.000-00" aria-label="Insira seu CPF" maxlength="15" aria-describedby="submit-button" required>
           <p id="label"></p>
         </div>
         <div class=" col-md-3  mx-auto">
